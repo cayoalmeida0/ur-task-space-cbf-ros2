afaca30f5af86172630fd63c9412d08ad7cac478
Cayo Sousa <cayoalmeida0@gmail.com>
2026-09-23 10:15:59 -0300

    Atualiza versao do pacote de controle

--- ur_cbf_ws/src/ur_cbf_control/setup.py
+++ ur_cbf_ws/src/ur_cbf_control/setup.py
@@ -6,7 +6,7 @@ package_name = "ur_cbf_control"
 
 setup(
     name=package_name,
-    version="0.6.32",
+    version="0.6.33",
     packages=find_packages(exclude=["test"]),
     data_files=[
         ("share/ament_index/resource_index/packages", [f"resource/{package_name}"]),
